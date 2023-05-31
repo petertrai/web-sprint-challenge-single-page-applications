d3ce43fc045e218703d00146c2cf3605934210c4
petertrai <petertrai@gmail.com>
2023-05-31 13:52:53 -0700

    random commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React from "react";
+import { Route, Routes, Link, useNavigate } from 'react-router-dom'
+import Form from './Form'
 
+const Home = (props) => {
+  const navigate = useNavigate()
+  const handlePizzaClick = () => {
+    navigate('/pizza')
+  }
+  return (
+    <div>
+      <h1>Welcome to Pizza Johns</h1>
+      <button onClick={handlePizzaClick} id='order-pizza'>Place Your Order</button>
+    </div>
+  )
+}
+
+const Pizzalink = (props) => {
+
+  return (
+    <div>
+      <h1>Pizza Form</h1>
+      <form id='pizza-form'>
+        <label>
+          Your Name
+          <input id='name-input' type='text' name='type'
+          />
+        </label>
+      </form>
+    </div>
+  )
+
+}
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <div>
+        <Link to='/'>Home</Link>&nbsp;
+
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/pizza' element={<Pizzalink />} />
+      </Routes>
     </>
   );
 };
---
+++ src/Form.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
+const Form = () => {
+    return (
+    <h1>Hi xD</h1>
+    )
+}
+
+export default Form();
